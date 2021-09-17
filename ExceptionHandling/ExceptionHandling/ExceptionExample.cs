68c9fa4eb088fd2980ef0ebd8061d8d10e9b74de
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-17 14:02:15 +0530

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/ExceptionExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class ExceptionExample : Exception
+    {
+        public ExceptionExample(string x)
+        {
+            Console.WriteLine(x + " This is User defined Exception");
+            Console.Read();
+
+        }
+    }
+
+
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Exception Handling ...");
+
+            int a = 2, b = 0;
+            int[] ar = null;
+            
+            try
+            {
+                //int c = a / b;
+                //Console.WriteLine(c);
+
+                //int x = ar[0];
+
+                //File.Open("sample.txt",FileMode.Open);
+
+                if (true)
+                    throw new ExceptionExample("Hello, ");
+
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("A number cannot divided by zero..");
+                Console.ReadLine();
+            }
+           
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Remove null Reference...");
+
+            }
+
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found the given path..");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("General Exception");
+            }
+
+            finally
+            {
+                Console.WriteLine("Finally block executed successfully..!");
+                Console.ReadLine();
+            }
+  
+            //Console.WriteLine("User defined Exception ");
+            //int i = 2;
+            //if (i == 2)
+            //    throw new ExceptionExample("Hi");
+
+        }
+    }
+
+}
